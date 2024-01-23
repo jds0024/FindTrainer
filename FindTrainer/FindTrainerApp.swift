8f24a02b07d338b1387c70f8e661a14385ad3908
Dasan <jds0024@naver.com>
2024-01-23 10:15:18 +0900

    first commit

---
+++ FindTrainer/FindTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindTrainerApp.swift
+//  FindTrainer
+//
+//  Created by 정다산 on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct FindTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
